6a18263580991a8eaa0cbc5f89b99feee722aee4
cbayonao <drcamilob@live.com>
2020-05-20 10:31:18 -0500

    [fix]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Search bad extension and repair
+
 exec { 'repair wp-settings':
   command => "sed -i 's/\\/class-wp-locale.phpp/\\/class-wp-local
               /var/www/html/wp-settings.php",
-  path    => '/bin/',
 }
