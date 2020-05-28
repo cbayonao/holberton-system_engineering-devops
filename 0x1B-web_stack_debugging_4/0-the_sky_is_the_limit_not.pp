70ab1d9b90d541251f924c4e7902b9d477ed6f34
cbayonao <drcamilob@live.com>
2020-05-28 12:41:05 -0500

    [Answers]

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Reapir NGNIX
+exec { 'repair NGINX':
+  command => "sed -i 's/15/4096/g' /etc/default/nginx",
+  path    => '/bin/',
+}
+
+service { 'repair NGINX':
+  ensure    => running,
+  subscribe => Exec['/etc/default/nginx'],
+}
